9ee263a6088eb1f8647623dbfb4a130418b75dba
omercnkc <250541117@firat.edu.tr>
2026-03-03 12:53:35 +0300

    chore: inital project setup with Vite +React + TS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Ömer ÇANAKÇI</p>
+      <p>Öğrenci No: 250541117</p>
+    </div>
+  );
+}
+
+export default App;
